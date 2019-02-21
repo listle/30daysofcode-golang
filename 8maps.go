cb2ff7982c1ae12251d459af13c84d61db39abd8
nadine <ianthe@gmail.com>
2019-02-20 20:23:05 -0600

    first commit

---
+++ 8maps.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+//asdfa
+
+func main() {
+	var num int
+	var input string
+	var m map[string]string
+	m = make(map[string]string)
+
+	fmt.Scan(&num)
+
+	for i := 0; i < num; i++ {
+		fmt.Scan(&input)
+		name := input
+
+		fmt.Scan(&input)
+		phone := strings.TrimSpace(input)
+		m[name] = phone
+	}
+
+	numinputs, _ := fmt.Scan(&input)
+
+	for numinputs > 0 {
+		if m[input] == "" {
+			fmt.Println("Not found")
+
+		} else {
+			fmt.Printf("%s=%s\n", input, m[input])
+		}
+		numinputs, _ = fmt.Scan(&input)
+	}
+
+}
